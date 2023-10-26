bfb4e224d550633f4dd56795fc78fec46326975f
jarltuxen <jarl.tuxen@gmail.com>
2023-10-26 20:10:52 +0200

    Soldiers

--- src/dk/kea/PredicateMain.java
+++ src/dk/kea/PredicateMain.java
@@ -2,6 +2,7 @@ package dk.kea;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,8 +26,13 @@ public class PredicateMain {
         System.out.println("High and StartsWithJ");
         Predicate<Soldier> startsWithJ = s -> s.getName().startsWith("J");
         Stream<Soldier> highAndJ = soldiers.stream().filter(isHighRanked).filter(startsWithJ);
-        highAndJ.forEach(s -> System.out.println(s));
+        //highAndJ.forEach(s -> System.out.println(s));
+        List<Soldier> highAndJList = highAndJ.collect(Collectors.toList());
+        System.out.println(highAndJList);
 
-        System.out.println(soldiers.stream().filter(startsWithJ).collect(Collectors.toList()));
+        System.out.println("Map");
+        Function<Soldier, String> getNames = soldier -> soldier.getName();
+        List<String> names = highAndJList.stream().map(getNames).collect(Collectors.toList());
+        highAndJList.stream().map(getNames).collect(Collectors.toList()).forEach(System.out::println);
     }
 }
