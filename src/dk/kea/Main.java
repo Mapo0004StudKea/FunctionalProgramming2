4bcd4922f43c14124334302b0b3871a2d9f0333f
jarltuxen <jarl.tuxen@gmail.com>
2023-10-26 16:06:29 +0200

    opg 4

--- src/dk/kea/Main.java
+++ src/dk/kea/Main.java
@@ -1,8 +1,87 @@
 package dk.kea;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Consumer<String> toUpper = x -> System.out.println(x.toUpperCase());
+        toUpper.accept("viggo");
+
+        Consumer<Long> square = t -> System.out.println(t*t);
+        square.accept(50l);
+
+        List<String> lstStr = Arrays.asList("hej", "du", "der");
+
+        forEachString(lstStr, toUpper);
+        forEach(lstStr, toUpper);
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Consumer<Integer> out = (Integer x) -> System.out.println(x);
+        forEach(list, out);
+
+        list.forEach(out);
+
+        ZeroOneTwoArgument.TwoArgument twoPlus = (x, y) -> x + y;
+        int ss = sumTwoArgFunction(list, twoPlus);
+        System.out.println(ss);
+
+        ZeroOneTwoArgument.TwoArgument twoMult = (x, y) -> x * y;
+        ss = sumTwoArgFunction(list, twoMult);
+        System.out.println(ss);
+
+        //Opret en liste af dollarsedler
+        List<Integer> dollarSedler = Arrays.asList(1,5,10);
+        int pengedk = valutaomregner(dollarSedler, twoMult, 6);
+        System.out.println("du har dk=" + pengedk);
+
+        //Decimal dollar
+        List<Double> dollars = Arrays.asList(1.0, 5.0, 10.0);
+        double pengedkdec = valutaomregnerG(dollars, (amount,kurs) -> (amount*kurs) , 6.55);
+        System.out.println("du har dk=" + pengedkdec);
     }
+
+    static void forEachString(List<String> listOfStrings, Consumer<String> consumer){
+        for (String s:listOfStrings){
+            consumer.accept(s);
+        }
+    }
+
+    static <T> void forEach(List<T> lst, Consumer<T> consumer) {
+        for (T t:lst){
+            consumer.accept(t);
+        }
+    }
+
+    static int sumTwoArgFunction(List<Integer> list, ZeroOneTwoArgument.TwoArgument two){
+        int sum = 0;
+        for (int t : list) {
+            sum += two.myFunc(t, t);
+        }
+        return sum;
+    }
+
+    static int valutaomregner(List<Integer> list, ZeroOneTwoArgument.TwoArgument two, int kurs) {
+        int sum = 0;
+        for (int t : list) {
+            sum += two.myFunc(t, kurs);
+        }
+        return sum;
+    };
+
+    public interface TwoArgumentG<T> {
+        public T myFunc(T a, T b);
+    }
+    static Double valutaomregnerG(List<Double> list, TwoArgumentG<Double> two, Double kurs) {
+        Double sum = 0.0;
+        for (Double d : list) {
+            sum += two.myFunc(d, kurs);
+        }
+        return sum;
+    };
+
+
+
 }
---
+++ src/dk/kea/ZeroOneTwoArgument.java
@@ -0,0 +1,42 @@
+package dk.kea;
+
+public class ZeroOneTwoArgument {
+    public static void main(String[] args) {
+        ZeroArgument z = () -> System.out.println("Hello I am interface");
+        printZero(z);
+
+        OneArgument out = (str) -> System.out.println(str);
+        out.myFunc("Hej one arg");
+
+        TwoArgument two = (x, y) -> x+y;
+        int i1 = two.myFunc(5,6);
+        System.out.println("i1=" + i1);
+
+        TwoArgument twomult = (x, y) -> x*y;
+        int i2 = twomult.myFunc(4,5);
+        System.out.println("i2=" + i2);
+
+        printOne("x", out);
+
+    }
+    public interface ZeroArgument {
+        public void myFunc();
+    }
+
+    public static void printZero(ZeroArgument z0){
+        z0.myFunc();
+    }
+
+    public interface OneArgument {
+        public void myFunc(String str);
+    }
+
+    public interface TwoArgument {
+        public int myFunc(int a, int b);
+    }
+
+    public static void printOne(String x, OneArgument z1){
+        z1.myFunc(x);
+    }
+
+}
